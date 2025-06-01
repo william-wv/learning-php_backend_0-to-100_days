ad1dfde1b92b8b2e9f392d800fd1666cee504b78
william-wv <wwillveiga@hotmail.com>
2025-06-01 19:57:06 -0300

    Endereço + toString

---
+++ endereco.php
@@ -0,0 +1,7 @@
+<?php
+
+use Alura\Banco\Modelo\Endereco;
+
+$umEndereco = new Endereco('Guarapuava','centro','Azevedo Portugal', '1264');
+
+echo $umEndereco . PHP_EOL;
--- src/Modelo/Endereco.php
+++ src/Modelo/Endereco.php
@@ -36,4 +36,9 @@ class Endereco
     {
         return $this->numero;
     }
+
+    public function __toString(): string
+    {
+        return "{$this->rua}, {$this->numero}, {$this->bairro}, {$this->cidade}";
+    }
 }
