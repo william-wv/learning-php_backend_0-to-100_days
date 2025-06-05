8a7501f9b2062ef2633cf8464d083ac803ed3fd2
william-wv <wwillveiga@hotmail.com>
2025-06-05 04:48:25 -0300

    initial commits day 005

--- src/Model/AcessoPropriedades.php
+++
@@ -1,9 +0,0 @@
-<?php  
-
-trait AcessoPropriedades {
-  public function __get(string $nomeAtributo)
-    {
-        $metodo = 'recupera' . ucfirst($nomeAtributo);
-        return $this->$metodo();
-    }
-}
